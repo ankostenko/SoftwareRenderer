a66569a4abb543a8a8c89514312df900a5ee3ace
ankostenko <andreykostenko99@gmail.com>
2019-12-01 21:54:56 +0300

    Infinite mouse movement, reduced object jittering

--- SoftwareRenderer/src/camera.cpp
+++ SoftwareRenderer/src/camera.cpp
@@ -35,13 +35,13 @@ struct FreeCamera {
 	float fov;
 	Mat4f view;
 	Mat4f proj;
-	float yaw = -90.0f;
+	float yaw = 270.0f;
 	float pitch = 0.0f;
 
 	int lastMouseY;
 	int lastMouseX;
 	float mouseSensitivity = 0.1f;
-	float movementSpeed = 1.0f;
+	float movementSpeed = 0.7f;
 
 	FreeCamera(float nearPlane, float farPlane, float fov, Vec3f pos = Vec3f({ 3.0f, 0.0f, 3.0f })) : position(pos), fov(fov), nearClippingPlane(nearPlane),
 		farClippingPlane(farPlane), worldUp(Vec3f({ 0.0f, 1.0f, 0.0f })) {
@@ -59,14 +59,17 @@ struct FreeCamera {
 	}
 	
 	void processMouseInput(int newMouseX, int newMouseY, float deltaTime) {
-		int xOffset = -(lastMouseX - newMouseX);
-		int yOffset = lastMouseY - newMouseY;
+		int xOffset = -(render.imagebuffer.width / 2 - newMouseX);
+		int yOffset =	render.imagebuffer.height / 2 - newMouseY;
 
-		yaw += (float)xOffset * mouseSensitivity * deltaTime;
+		yaw +=   (float)xOffset * mouseSensitivity * deltaTime;
 		pitch += (float)yOffset * mouseSensitivity * deltaTime;
 
-		lastMouseX = newMouseX;
-		lastMouseY = newMouseY;
+		// Constrains
+		if (yaw >= 360.0f) { yaw = 0; }
+		if (yaw < 0) { yaw = 360; }
+		if (pitch >= 89.0f)  { pitch = 89.0f; }
+		if (pitch <= -89.0f) { pitch = -89.0f; }
 	}
 
 	void processMouseScrolling(Mouse mouse) {
